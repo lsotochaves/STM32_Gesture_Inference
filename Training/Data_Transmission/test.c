0e42c15322feaafdfc06159991b2a5a04147efd8
lsotochaves <luis.sotochaves@ucr.ac.cr>
2026-06-29 16:37:39 -0600

    debug

--- Training/Data_Transmission/test.c
+++ Training/Data_Transmission/test.c
@@ -1,4 +1,6 @@
 #include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/spi.h>
 
 #include <libopencm3-plus/newlib/syscall.h>
 #include <libopencm3-plus/newlib/devices/cdcacm.h>
@@ -16,8 +18,27 @@ int main(void) {
     setvbuf(stdin,  NULL, _IONBF, 0);
     setvbuf(stdout, NULL, _IONBF, 0);
 
+    printf("PASO 0\r\n");
+
+    rcc_periph_clock_enable(RCC_SPI5);
+    rcc_periph_clock_enable(RCC_GPIOF);
+    gpio_mode_setup(GPIOF, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO7 | GPIO8 | GPIO9);
+    gpio_set_af(GPIOF, GPIO_AF5, GPIO7 | GPIO8 | GPIO9);
+    rcc_periph_clock_enable(RCC_GPIOC);
+    gpio_set(GPIOC, GPIO1);
+    gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO1);
+    spi_set_master_mode(SPI5);
+    spi_set_baudrate_prescaler(SPI5, SPI_CR1_BR_FPCLK_DIV_16);
+    spi_enable_software_slave_management(SPI5);
+    spi_set_nss_high(SPI5);
+    spi_set_dff_8bit(SPI5);
+    spi_set_clock_polarity_1(SPI5);
+    spi_set_clock_phase_1(SPI5);
+    spi_enable(SPI5);
+
+    printf("PASO 1\r\n");
+
     while (1) {
-        printf("PASO 0\r\n");
         for (volatile int i = 0; i < 1000000; i++);
     }
 }
